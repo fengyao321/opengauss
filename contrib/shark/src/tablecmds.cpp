1fd017b1676e7cc5c9a38a009e8dd31540fb7e80
shijuzheng1997 <shijuzheng1997@126.com>
2026-01-08 19:10:15 +0800

    fix liscence

--- contrib/shark/src/tablecmds.cpp
+++ contrib/shark/src/tablecmds.cpp
@@ -1,9 +1,23 @@
-/* -------------------------------------------------------------------------
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2026. All rights reserved.
+ *
+ * openGauss is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * --------------------------------------------------------------------------------------
  *
  * tablecmds.cpp
  *      Shark's Commands for creating and altering table structures and settings
  *
- * Portions Copyright (c) 2020 Huawei Technologies Co.,Ltd.
+ * Portions Copyright (c) 2026, Huawei Technologies Co.,Ltd.
+ * Portions Copyright (c) 2020, AWS
  * Portions Copyright (c) 1996-2012, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  * Portions Copyright (c) 2010-2012 Postgres-XC Development Group
