a3155d8da8ef6d90d058678bb555232abc27f76f
wangfeihuo <wangfeihuo@huawei.com>
2026-01-20 11:18:44 +0800

    修复部分视图sysadmin查询时报错的问题

--- contrib/shark/rollback_script/shark--3.0--2.0.sql
+++ contrib/shark/rollback_script/shark--3.0--2.0.sql
@@ -8,6 +8,46 @@ drop view if exists sys.tables;
 drop function if exists sys.ts_procedure_object_internal;
 drop function if exists sys.ts_tables_obj_internal;
 
+drop view if exists sys.all_columns;
+create or replace view sys.all_columns as
+select
+  a.attrelid as object_id,
+  a.attname as name,
+  cast(a.attnum as int) as column_id,
+  a.atttypid as system_type_id,
+  a.atttypid as user_type_id,
+  sys.tsql_type_max_length_helper(t.typname, a.attlen, a.atttypmod) as max_length,
+  sys.ts_numeric_precision_helper(t.typname, a.atttypmod) as precision,
+  sys.ts_numeric_scale_helper(t.typname, a.atttypmod) as scale,
+  coll.collname as collation_name,
+  cast(case a.attnotnull when 't' then 0 else 1 end as bit) as is_nullable,
+  cast(0 as bit) as is_ansi_padded,
+  cast(0 as bit) as is_rowguidcol,
+  cast(0 as bit) as is_identity,
+  cast(case when d.adgencol = 'p' then 1 else 0 end as bit) as is_computed,
+  cast(0 as bit) as is_filestream,
+  sys.ts_is_publication_helper(a.attrelid) as is_replicated,
+  cast(0 as bit) as is_non_sql_subscribed,
+  cast(0 as bit) as is_merge_published,
+  cast(0 as bit) as is_dts_replicated,
+  cast(0 as bit) as is_xml_document,
+  cast(0 as oid) as xml_collection_id,
+  d.oid as default_object_id,
+  cast(0 as int) as rule_object_id,
+  cast(0 as bit) as is_sparse,
+  cast(0 as bit) as is_column_set,
+  cast(0 as tinyint) as generated_always_type,
+  cast('NOT_APPLICABLE' as nvarchar(60)) as generated_always_type_desc
+from pg_attribute a
+inner join pg_class c on c.oid = attrelid
+inner join pg_namespace s on s.oid = c.relnamespace
+inner join pg_type t on t.oid = a.atttypid
+left join pg_attrdef d on a.attrelid = d.adrelid and a.attnum = d.adnum
+left join pg_collation coll on coll.oid = a.attcollation
+where not a.attisdropped and a.attnum > 0
+and c.relkind in ('r', 'v', 'm', 'f')
+and has_column_privilege(quote_ident(s.nspname) ||'.'||quote_ident(c.relname), a.attname, 'SELECT');
+
 create or replace function sys.ts_tables_obj_internal()
 returns table (
 	out_name name,
